6a148e383d5069a9265666833ea7f5fc9d6e54b0
jbcoliveira <jbcoliveira1984@gmail.com>
2021-09-21 16:08:04 -0300

    Commit inicial

---
+++ 01ASPNetCore/CustomMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace _01ASPNetCore
+{
+
+    public class CustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine("\n\r --- Before --- \n\r");
+
+            await _next(context);
+
+            Console.WriteLine("\n\r --- After --- \n\r");
+        }
+    }
+
+    public static class CustomMiddlewareExtension
+    {
+        public static IApplicationBuilder UseCustomMiddleware(this IApplicationBuilder builder) 
+        {
+            return builder.UseMiddleware<CustomMiddleware>();
+        }
+    }
+}
